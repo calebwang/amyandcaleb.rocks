158531ca4719bfb14d03f5418d4cc82953329666
Amy Tang <amytang0@gmail.com>
2023-08-10 16:45:02 -0700

    Basic mobile fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Amy & Caleb's Van Life"
+    title: "Amy & Caleb's Van Life",
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' 
 }
 
 export default function RootLayout({
