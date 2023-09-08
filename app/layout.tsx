41dbc2cca000928d2cda08c118dab1b9fd4d4887
caleb <caleb@caleb.wang>
2023-09-08 13:18:04 -0700

    Make mobile friendly

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,16 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
